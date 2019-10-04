47941beb6d717dd730ef7abfec1bf6b42314c9ef
anant007 <anant.awasthy29@gmail.com>
2019-10-04 12:30:41 +0530

    done

--- config/database.js
+++ config/database.js
@@ -1,27 +1,5 @@
 const Sequelize = require('sequelize')
 
 
-// const _p = new Pool({
-//     user : 'postgres',
-//     password : '123123123',
-//     host : 'localhost',
-//     port : 5432,
-//     database : 'quonquer'
-// })
-
-
-// module.exports = new Sequelize('codinggigs', 'postgres', 'password', {
-//     host: 'db',
-//     dialect: 'postgres',
-//     operatorsAlias : false,
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//       },
-      
-// });
-
-
 module.exports =  new Sequelize('postgres://postgres:password@db:5432/codinggigs');
+//module.exports =  new Sequelize('postgres://postgres:123123123@localhost:5432/quonquer');
