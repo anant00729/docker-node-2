49147bb9e4197ea68a63b633ba100e500c51d538
anant007 <anant.awasthy29@gmail.com>
2019-10-23 15:29:11 +0530

    done

---
+++ public/build/precache-manifest.7b7a1b8250b3af5471bc099532ca8ef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb992615e45e444e916d678dc86feb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60d34abc2afe28af166",
+    "url": "/static/css/main.56f8ac9a.chunk.css"
+  },
+  {
+    "revision": "0c0e3d5d01424345da2b",
+    "url": "/static/js/2.b20676d5.chunk.js"
+  },
+  {
+    "revision": "d60d34abc2afe28af166",
+    "url": "/static/js/main.bb3ad98a.chunk.js"
+  },
+  {
+    "revision": "1744440255e2904d24c1",
+    "url": "/static/js/runtime~main.b047a503.js"
+  },
+  {
+    "revision": "e9645c264ecd6c30d8f7debff1ff882c",
+    "url": "/static/media/a-det-sigin-up.e9645c26.png"
+  },
+  {
+    "revision": "82f6de727613cb08ea60c9ff70638953",
+    "url": "/static/media/app_img.82f6de72.jpg"
+  }
+]);
