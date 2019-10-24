230786e6cda9e29aa45bbc5a74e22ca61fe66479
anant007 <anant.awasthy29@gmail.com>
2019-10-24 08:55:21 +0530

    added build folter

---
+++ public/build/precache-manifest.a866af6d89e22c2a51899c737d58bccb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8538458dc4f7b678f2be2e53d44ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b1c739e7fb8b4c1efac",
+    "url": "/static/css/main.56f8ac9a.chunk.css"
+  },
+  {
+    "revision": "0993943b003f6f3a1b58",
+    "url": "/static/js/2.1d4e3be9.chunk.js"
+  },
+  {
+    "revision": "5b1c739e7fb8b4c1efac",
+    "url": "/static/js/main.77fd9f3a.chunk.js"
+  },
+  {
+    "revision": "1744440255e2904d24c1",
+    "url": "/static/js/runtime~main.b047a503.js"
+  },
+  {
+    "revision": "e9645c264ecd6c30d8f7debff1ff882c",
+    "url": "/static/media/a-det-sigin-up.e9645c26.png"
+  },
+  {
+    "revision": "82f6de727613cb08ea60c9ff70638953",
+    "url": "/static/media/app_img.82f6de72.jpg"
+  }
+]);
